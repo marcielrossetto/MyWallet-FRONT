d82924814c15b6d54696ba22c470bda9d110c2f6
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2025-05-20 00:54:42 -0300

    troca url para testar render2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:4000";
-// const API_URL = "https://mywallet-backend-qmpl.onrender.com";
+
+const API_URL = "https://mywallet-backend-qmpl.onrender.com";
 
 let token = "";
 let editingTransaction = null;
